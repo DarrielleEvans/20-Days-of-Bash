e6430d37ad62ab772c787a31051439e0a92787de
Darrielle Evans <89504317+DarrielleEvans@users.noreply.github.com>
2023-09-25 19:29:11 -0500

    Update file_exist.sh

--- file_exist.sh
+++ file_exist.sh
@@ -1,1 +1,36 @@
+#!/bin/bash
+# Darrielle Evans
+# September 25, 2023
+# Purpose: To check if a file exists in the current directory
 
+
+path=$(pwd)
+
+while true 
+do
+    read -p "Please enter the name of the file you are looking for: " fileName
+
+    if [ "$fileName" == "" ]
+    then
+        echo "Please enter a correct file name or enter exit if you want to quit"
+        sleep 5
+        if [ "$fileName" == "exit" ]; then
+            break
+        fi
+        
+    elif [ -f $path/$fileName ]; then
+        read -p "File Exists. Enter continue if you want to search again or exit if you are done." nextAction
+        if [ $nextAction == "continue" ]; then
+            continue
+        else
+            break
+        fi
+    else 
+        read -p "File Does Not Exist. Enter continue if you want to search again or exit if you are done." nextAction
+        if [ $nextAction == "continue" ]; then
+            continue
+        else
+            break
+        fi
+    fi
+done
